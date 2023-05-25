05c03332f2bdf88645eb85e7636448cb9e71bc02
Arninator <sechzehngeteiltdurchneun@gmx.de>
2023-05-25 17:32:35 +0200

    try something without effect

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,6 +4,8 @@ import { Link } from "gatsby";
 const Logo = () => {
   // const [isActive, setIsActive] = useState(false);
 
+  console.log(document.documentElement.scrollHeight);
+
   return (
     <nav
       className="navbar"
@@ -14,12 +16,12 @@ const Logo = () => {
         position: "absolute",
         top: "0",
         width: "40%",
-        height: typeof document != "undefined" ? document.documentElement.scrollHeight : "100%",
+        height: typeof document === 'undefined' ? "100%" : document.documentElement.scrollHeight,
       }}
     >
-      <div className="navbar-brand has-background-white">
+      <div className="navbar-brand is-flex is-flex-direction-column is-justify-content-flex-start">
         <Link to="/" className="navbar-item" title="Logo">
-          <div className="has-text-black mx-6" style={{ fontSize: "5rem", fontWeight: "300"}}>Marie Breloer</div>
+          <div className="has-text-black mx-6" style={{ fontSize: "5rem", fontWeight: "100"}}>marie breloer</div>
         </Link>
       </div>
     </nav>
